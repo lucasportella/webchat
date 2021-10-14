d66316fa8f12563ba186bae3f5a701d095735e59
lucas <lucasportellaagu@gmail.com>
2021-10-14 18:34:32 -0300

    basic socket connection

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 const express = require('express');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: `http://localhost:${PORT}`,
+        methods: ['GET', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log(`${socket.id} conectado!`);
+});
+
+app.get('/', (req, res) => {
+    res.render(path.join(__dirname, 'index.ejs'));
+});
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+http.listen(PORT, () => {
+    console.log(`Ouvindo na porta ${PORT}`);
+});
