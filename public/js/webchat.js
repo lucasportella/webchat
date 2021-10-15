7dcece0d13274f0e70ba62e29d9ce906338aca20
lucas <lucasportellaagu@gmail.com>
2021-10-15 12:03:48 -0300

    messages exchanges rendering on html

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -1,8 +1,15 @@
 const socket = window.io();
 
+const ul = document.querySelector('ul');
 const form = document.querySelector('form');
 const input = document.querySelector('#textInput');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     socket.emit('message', input.value);
+});
+
+socket.on('message-return', (message) => {
+    const li = document.createElement('li');
+    li.innerText = message;
+    ul.appendChild(li);
 });
--- socket/webchat.js
+++ socket/webchat.js
@@ -1,5 +1,9 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log(`${socket.id} conectado!`);
+
+        socket.on('message', (message) => {
+            io.emit('message-return', message);
+        });
     });
 };
