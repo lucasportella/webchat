0fb428ce0a5d74be5dc0c8367118e99b5b644e9a
lucas <lucasportellaagu@gmail.com>
2021-10-15 12:22:40 -0300

    formatted message part 1

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -5,11 +5,11 @@ const form = document.querySelector('form');
 const input = document.querySelector('#textInput');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    socket.emit('message', input.value);
+    socket.emit('message', { chatMessage: input.value, nickname: socket.id });
 });
 
-socket.on('message-return', (message) => {
+socket.on('message-return', (formattedMessage) => {
     const li = document.createElement('li');
-    li.innerText = message;
+    li.innerText = formattedMessage;
     ul.appendChild(li);
 });
--- socket/webchat.js
+++ socket/webchat.js
@@ -2,8 +2,10 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log(`${socket.id} conectado!`);
 
-        socket.on('message', (message) => {
-            io.emit('message-return', message);
+        socket.on('message', (msgObject) => {
+            const { chatMessage, nickname } = msgObject;
+            const formattedMessage = `- ${nickname}: ${chatMessage}`;
+            io.emit('message-return', formattedMessage);
         });
     });
 };
