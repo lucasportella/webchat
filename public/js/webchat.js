d878d2dbac1609c3f1efdce9ed10d705844a6982
lucas <lucasportellaagu@gmail.com>
2021-10-15 10:10:07 -0300

    creating message feature through eventListener

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -1,1 +1,8 @@
 const socket = window.io();
+
+const form = document.querySelector('form');
+const input = document.querySelector('#textInput');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    socket.emit('message', input.value);
+});
